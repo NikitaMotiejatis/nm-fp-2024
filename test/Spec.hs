ae56825b8d8b806d3d104958541d3713d3bd1ef9
Slava Pozdniakov <viaceslav.pozdniakov@vinted.com>
2024-11-05 17:34:38 +0200

    Add QC example

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
-import Test.Tasty (TestTree, defaultMain, testGroup)
-import Test.Tasty.HUnit (testCase, (@?=))
+import Test.Tasty ( TestTree, defaultMain, testGroup )
+import Test.Tasty.HUnit ( testCase, (@?=) )
+import Test.Tasty.QuickCheck as QC
+
+import Data.List
+import Data.Ord
 
 import Lib2
 
@@ -9,47 +12,21 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [unitTests]
+tests = testGroup "Tests" [unitTests, propertyTests]
 
 unitTests :: TestTree
-unitTests = testGroup "Lib2 tests"
-  [ 
-    testCase "Parsing rent command" $
-      parseQuery "rent user1 bike1" @?=
-        Right (RentStmt (UserID 1) (BikeID 1)),
+unitTests = testGroup "Lib1 tests"
+  [ testCase "List of completions is not empty" $
+      null Lib1.completions @?= False,
+    testCase "Parsing case 1 - give a better name" $
+      Lib2.parseQuery "" @?= (Left "Some error message"),
+    testCase "Parsing case 2 - give a better name" $
+      Lib2.parseQuery "o" @?= (Left "Some error message")
+  ]
 
-    testCase "Parsing multiple commands" $
-      parseQuery "register user user1; register bike bike1; rent user1 bike1" @?=
-        Right (Sequence [ RegisterUserStmt (UserID 1),
-                          RegisterBikeStmt (BikeID 1),
-                          RentStmt (UserID 1) (BikeID 1) ]),
-
-    testCase "Parsing with extra whitespace" $
-      parseQuery "  register   bike   bike5  " @?=
-        Right (RegisterBikeStmt (BikeID 5)),
-
-    testCase "Parsing invalid command" $
-      case parseQuery "invalid command" of
-        Left _  -> () @?= ()
-        Right _ -> error "Expected parsing to fail",
-
-    testCase "State transition - check user after renting a bike" $
-      let initialState = emptyState
-          Right (_, state1) = stateTransition initialState (RegisterUserStmt (UserID 1))
-          Right (_, state2) = stateTransition state1 (RegisterBikeStmt (BikeID 1))
-          Right (_, state3) = stateTransition state2 (RentStmt (UserID 1) (BikeID 1))
-          Right (Just msg, _) = stateTransition state3 (CheckUserStmt (UserID 1))
-      in msg @?= "user1 has rented: bike1",
-
-    testCase "State transition - register user" $
-      let initialState = emptyState
-          Right (Just msg, state1) = stateTransition initialState (RegisterUserStmt (UserID 1))
-      in do
-          msg @?= "User registered successfully."
-          state1 @?= initialState { users = [UserID 1] },
-
-    testCase "State transition - rent bike without registration" $
-      case stateTransition emptyState (RentStmt (UserID 1) (BikeID 1)) of
-        Left err -> err @?= "User not registered."
-        Right _  -> error "Expected operation to fail"
+propertyTests :: TestTree
+propertyTests = testGroup "some meaningful name"
+  [
+    QC.testProperty "sort == sort . reverse" $
+      \list -> sort (list :: [Int]) == sort (reverse list)
   ]
